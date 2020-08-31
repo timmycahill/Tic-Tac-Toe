1525c74ceff972dfc66501cc949bc5883916a3b9
Timothy Cahill <tcahill17@hotmail.com>
2020-08-31 14:03:26 -0400

    Updated GUI to play a game (does not detect game over)

--- tictactoe.py
+++ tictactoe.py
@@ -37,22 +37,108 @@ class TicTacToe:
 
 		# Print empty squares where they would be on the board
 		for i in range(3):
-			self.emptyRect.move_ip(i * 205, 0)
 			for j in range(3):
-				self.emptyRect.move_ip(0, j * 205)
-				self.screen.blit(self.empty, self.emptyRect)
-				pygame.display.update()
-				self.emptyRect.move_ip(0, j * (-205))
+				self._draw_box(self.empty, self.emptyRect, i, j)
 
-			self.emptyRect.move_ip(i * (-205), 0)
 
 	def _main_loop(self):
-		clock = pygame.time.Clock()
 		while True:
-			clock.tick(60)
+			self._game_loop()
+			if not self._play_again():
+				break
 
+	def _play_again(self):
+		while True:
+			playAgain = input("Would you like to play again? (Y/N): ")
+			if playAgain.upper() == "Y":
+				return True
+			elif playAgain.upper() == "N":
+				return False
+			print("Invalid input. Please try again.")
+
+	def _draw_box(self, image, rect, xloc, yloc):
+		xloc *= 205
+		yloc *= 205
+		rect.move_ip(xloc, yloc)
+		self.screen.blit(image, rect)
+		rect.move_ip(-xloc, -yloc)
+		pygame.display.update()
+
+	def _game_loop(self):
+		gameOver = False
+		if random.randint(0, 1) == 0:
+			self.turn = 'x'
+		else:
+			self.turn = 'o'
+
+		while not gameOver:
+			if self.turn == 'x':
+				self._player_turn()
+				self.turn = 'o'
+			else:
+				self._computer_turn()
+				self.turn = 'x'
+			self._check_for_winner()
+
+	def _player_turn(self):
+		validSelection = False
+
+		while not validSelection:
+			selection = self._player_selection()
+			validSelection = self._validate_selection(selection)
+			if validSelection:
+				self._finalize_selection(selection)		
+
+	def _player_selection(self):
+		while True:
 			for event in pygame.event.get():
+				# If player exits...
 				if event.type == QUIT:
-					return
+					sys.exit()
+				# If mouse is clicked...
 				elif event.type == MOUSEBUTTONDOWN:
-					print(pygame.mouse.get_pos)
+					# Read in mouse position
+					x, y = pygame.mouse.get_pos()
+
+					# Determine which box was clicked
+					for i in range(3):
+						finished = False
+						for j in range(3):
+							if x < (200 + (205 * i)) and y < (200 + (205 * j)):
+								return i, j
+
+	def _validate_selection(self, selection):
+		x, y = selection
+
+		return self.board[x][y] == None
+
+	def _finalize_selection(self, selection):
+		x, y = selection
+
+		self.board[x][y] = self.turn
+
+		if self.turn == 'x':
+			image = self.x
+			rect = self.xRect
+		else:
+			image = self.o
+			rect = self.oRect
+
+		self._draw_box(image, rect, x, y)
+
+	def _computer_turn(self):
+		validSelection = False
+
+		while not validSelection:
+			selection = self._randomize_selection()
+			validSelection = self._validate_selection(selection)
+			if validSelection:
+				self._finalize_selection(selection)
+
+	def _randomize_selection(self):
+		x = random.randint(0,2)
+		y = random.randint(0,2)
+		return x, y
+
+	def _check_for_winner(self):
+		pass
